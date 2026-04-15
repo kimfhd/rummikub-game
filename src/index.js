3d61682f00e91c309790f077bc000f905338d3ed
kimfhd <fhd13245342@gmail.com>
2026-04-15 16:24:54 +0800

    yxf

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // 如果你没有这个文件，可以先把这行删掉
 import App from './App'; // 这会自动寻找同目录下的 App.jsx 或 App.js
 
 // 找到 index.html 中 id="root" 的元素
